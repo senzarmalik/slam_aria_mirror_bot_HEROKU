9d803d19bb59ce48d219314b1efdc19c907a109c
senzarmalik <87890139+senzarmalik@users.noreply.github.com>
2021-08-05 21:11:24 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -47,7 +47,7 @@ def stats(update, context):
             f'<b>├  🎮 RAM : {memory}%</b>\n' \
             f'<b>├  💽 DISK : {disk}%</b>\n' \
             f'<b>│</b>\n' \
-            f'<b>╰──「 🚸 @AT_BOTs 🚸 」</b>'
+            f'<b>╰──「 🚸 @C717R_G067R 🚸 」</b>'
     update.effective_message.reply_photo(IMAGE_URL, stats, parse_mode=ParseMode.HTML)
 
 
